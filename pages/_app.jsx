99948409ac8f84f9b36b62083b398922ad417ec0
TASLIM-ENIOLLA <taslim.eniolla@gmail.com>
2022-10-30 08:31:21 +0100

    Second commit 10/29/2022

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,13 @@
 import {Fragment} from 'react'
+import Head from 'next/head'
 import '/public/css/globals.css'
 
 export default function App({Component, pageProps}){
     return (
         <Fragment>
+            <Head>
+                <title>Sally Hostel</title>
+            </Head>
             <Component {...pageProps} />
             <div id = '__popup'></div>
         </Fragment>
